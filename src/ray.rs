bfcd1c754ec6c453d2c15f6e8af42c5ec0e17560
PsuperX <ja435364@gmail.com>
2023-09-22 10:10:09 +0100

    Sphere

--- src/ray.rs
+++ src/ray.rs
@@ -1,5 +1,5 @@
 use crate::color::Color;
-use glam::DVec3;
+use glam::{dvec3, DVec3};
 
 pub struct Ray {
     pub orig: DVec3,
@@ -16,8 +16,21 @@ impl Ray {
     }
 
     pub fn color(&self) -> Color {
+        if self.hit_sphere(dvec3(0., 0., -1.), 0.5) {
+            return Color::new(1., 0., 0.);
+        }
+
         let unit_dir = self.dir.normalize();
         let a = 0.5 * (unit_dir.y + 1.);
         Color::lerp(Color::ONE, Color::new(0.5, 0.7, 1.), a)
+    }
+
+    pub fn hit_sphere(&self, center: DVec3, radius: f64) -> bool {
+        let ac = self.orig - center;
+        let a = self.dir.dot(self.dir);
+        let b = 2. * self.dir.dot(ac);
+        let c = ac.dot(ac) - radius * radius;
+        let discriminant = b * b - 4. * a * c;
+        discriminant >= 0.
     }
 }
